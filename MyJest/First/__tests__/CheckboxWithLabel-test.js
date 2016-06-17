84d98fcc17aab8a67c41fca743a9070fe72fad96
何柯 <heke@hujiang.com>
2016-06-17 15:52:01 +0800

    update

--- MyJest/First/__tests__/CheckboxWithLabel-test.js
+++ MyJest/First/__tests__/CheckboxWithLabel-test.js
@@ -9,7 +9,7 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import CheckboxWithLabel from '../CheckboxWithLabel';
 
-describle('CheckboxWithLabel', ()=> {
+describe('CheckboxWithLabel', ()=> {
     it('changes the text after click', ()=> {
         const checkbox = TestUtils.renderIntoDocument(
             <CheckboxWithLabel labelOn="On" labelOff="Off"/>
